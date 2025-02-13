12c620da85a7b2c94afbd4a20785a2f4009bd535
fa6om <fa6omi55@gmail.com>
2025-02-13 11:00:59 +0300

    solved

--- iterationMethods.js
+++ iterationMethods.js
@@ -4,31 +4,42 @@ const numbers = [10, 13, 20, 25, 38, 35, 40];
 // 1) Using `greaterThanTwentyFive` function that returns a new array containing numbers that are greater than or equal to 25.
 const greaterThanTwentyFive = (numbers) => {
   // write your code here...
+
+  return numbers.filter((number) => number >= 25);
 };
 
 // 2) Using `divisibleByFive` function return a new array containing numbers that are divisible by 5.
 const divisibleByFive = (numbers) => {
   // write your code here...
+  return numbers.filter((number) => number % 5 === 0);
 };
 
 // 3) Using `squaredNumbers` function return a new array that contains each number squared.
 const squaredNumbers = (numbers) => {
   // write your code here...
+
+  return numbers.map((number) => number ** 2);
 };
 
 // 4) Using `doubledNumbers` function return a new array that contains each number multiplied by 2.
 const doubledNumbers = (numbers) => {
   // write your code here...
+  return numbers.map((number) => number * 2);
 };
 
 // 5) Using `filteredAndSquared` function return the numbers that are greater than or equal to 20 and then square each of them.
 const filteredAndSquared = (numbers) => {
   // write your code here...
+  return numbers.filter((number) => number >= 20).map((number) => number ** 2);
 };
 
 // 6) Using `filteredAndTripled` return the numbers that are divisible by 5 and then multiply each of them by 3.
 const filteredAndTripled = (numbers) => {
   // write your code here...
+
+  return numbers
+    .filter((number) => number % 5 === 0) // Filter numbers divisible by 5
+    .map((number) => number * 3);
 };
 
 // 🌶️🌶️🌶️ Extra
